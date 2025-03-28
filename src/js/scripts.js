e13e857890b251cdb42f9456dba48dbc839d7657
Shuyue Xu <shuyuexu@bu.edu>
2025-03-28 17:59:34 -0400

    Show/hide paragraph

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,26 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "Shuyue Xu";
+const myAge = 20;
+
+console.log(myAge);
+
+//Practice 3 begin
+
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+//Write a function that toggles the show/hide class
+function toggleParagraph() {
+    toggledParagraph.classList.toggle("hide");
+    toggledParagraph.classList.toggle("show");
+    console.log("My toggle function");
+    console.log(toggledParagraph);
+}
+
+//Find the button and make something when we click it
+const toggleButton = document.querySelector("#toggle-control");
+
+toggleButton.addEventListener("click", toggleParagraph);
